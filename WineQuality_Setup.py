63caf7e9c3b0b34102402c72ec9870009886a5a6
Glenn Wiebe <Glenn.Wiebe@databricks.com>
2021-11-20 13:07:08 -0500

    Re-org based on libraries

---
+++ WineQuality_Setup.py
@@ -0,0 +1,72 @@
+# Databricks notebook source
+# MAGIC %md ## Setup notebook for prototype: create database, tracking table, and set common variables, etc.
+# MAGIC   
+# MAGIC Defaults to not doing this, so you can run by default without damaging existing.
+
+# COMMAND ----------
+import json
+from datetime import datetime
+# import local functions
+from utils.setup_functions import *
+
+# COMMAND ----------
+
+# TODO resolve these "create" widgets with "reset" widget 
+# Setup widgets for General Setup Process
+dbutils.widgets.dropdown("SetupType", 'new',['new','clear','reset'],"Setup DB Options")
+dbutils.widgets.dropdown("CreateTable", "No", ["Yes", "No"], "Create Tracking Table")
+
+# Setup widgets for Setup Config
+dbutils.widgets.text("store_loc", default_user_path, "User Storage Location")
+dbutils.widgets.text("db_name", db_name, "Database Name")
+dbutils.widgets.text("track_table_name", track_table_name, "Track Table Name")
+
+# COMMAND ----------
+
+# Setup variables separate from current widget
+db_name = dbutils.widgets.get("db_name")
+track_table_name = dbutils.widgets.get("track_table_name")
+store_loc = dbutils.widgets.get("store_loc")
+SetupType = dbutils.widgets.get("SetupType")
+
+# Remove widgets as necessary
+# dbutils.widgets.remove("db_name")
+
+
+# COMMAND ----------
+
+# First get component to initialize environment (user and notebook name)
+set_env(store_loc,db_name)
+
+# COMMAND ----------
+
+# Create DB
+if SetupType == "new":
+  print("Setting up / creating new database {db_name} in storage location {db_loc}...".format(db_name,db_loc))
+  create_db(db_loc,db_name)
+
+if dbutils.widgets.get("CreateTable") == "Yes":
+  print("Setting up load tracking table...")
+  create_track_table(db_name,track_table_name)
+
+print("Done!")
+
+# drop_table(use_db,"quality_red_bronze")
+
+
+notebook_path_json = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson())
+current_notebook = notebook_path_json['extraContext']['notebook_path']
+
+current_user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+default_user_path = "/Users/{}/".format(current_user)
+db_name = "ggw_wine"
+track_table_name = "load_track"
+file_path = "file:/Workspace/Repos/glenn.wiebe@databricks.com/dbx-data-quality/data/"
+measure_usecase_tag = "data_load" 
+
+load_start_dt = datetime.now()
+
+print("config setup:")
+print("  current_notebook = {}\n      current_user = {},\n default_user_path = {}".format(current_notebook,current_user,default_user_path))
+print("         file_path = {}".format(file_path))
+print("           db_name = {}\n  track_table_name = {}\n     load_start_dt = {}".format(db_name,table_name,load_start_dt))
---
+++ utils/setup_functions.py
@@ -0,0 +1,65 @@
+### MAGIC %run ../config/setup
+
+# import common librarys
+import json
+from datetime import datetime
+
+# Function to setup component name variable
+def set_env(store_loc,db_name):
+  print("get_component called: finding data location={db_name}...".format(db_loc,db_name))
+  tags = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags()
+  print(tags)
+
+  data_file_loc = "{}{}".format(store_loc,db_name) 
+  db_loc = "{}{}/{}.db".format(store_loc,db_name,db_name) 
+
+  print("Using...\n    store location = {store_loc}\n            Database = {db_name},\n       db location = {store_loc}  \n db storage location = {}...".format(store_loc,db_name,data_file_loc,db_loc))
+
+# Function to setup database
+def create_db(db_name,db_loc):
+  print("create_db called: creating storage location, db_loc={db_loc}, and creating db, db_name={db_name}...".format(db_loc,db_name))
+  dbutils.fs.mkdirs(db_loc)
+  spark.sql(f"""CREATE DATABASE {db_name}
+    LOCATION '{db_loc}'
+  """)
+  display(dbutils.fs.ls(data_file_loc))
+
+def clear_db(db_name,db_loc):
+  print("clear_db called: droping db, db_name={db_name}, removing and storage location={db_loc}...".format(db_name,db_loc))
+  spark.sql(f"DROP DATABASE IF EXISTS {db_name} CASCADE")
+  dbutils.fs.rm(db_loc, True)
+  display(dbutils.fs.ls(data_file_loc))
+
+def reset_db(db_name,db_loc):
+  print("reset_db called: clearing storage directory: {db_loc}, and creating database (if not exists) {db_name}...".format(db_loc,db_name))
+  dbutils.fs.rm(data_file_loc, False)
+  spark.sql(f"""CREATE DATABASE IF NOT EXISTS {db_name} LOCATION '{db_loc}' """)
+  spark.sql(f"""USE {db_name}""")
+
+  display(dbutils.fs.ls(data_file_loc))
+
+# Function to setup database
+def create_track_table(db_name,track_table_name):
+  print("setup_track_table called: dropping existing load tracking table {}.{}".format(db_name,track_table_name))
+  spark.sql("""DROP TABLE IF EXISTS {db_name}.{track_table_name}""".format(db_name,track_table_name))
+  print("                          re-creating load tracking table {}.{}".format(db_name,track_table_name))
+  spark.sql("""CREATE TABLE IF NOT EXISTS {db_name}.{track_table_name} (
+                 load_start_dt TIMESTAMP,
+                 load_end_dt TIMESTAMP,
+                 load_process STRING,
+                 load_source STRING,
+                 load_target STRING,
+                 load_count LONG,
+                 load_checksum DOUBLE,
+                 load_metrics STRING
+               )""".format(db_name,track_table_name)
+           )
+  print("                          load tracking table created: {db_name}.{track_table_name}".format(db_name, track_table_name))
+
+# Drop Table utility function
+def drop_table(db_name,table_name):
+  print("drop_table called: dropping table: {}.{}...".format(db_name,table_name))
+  spark.sql("DROP TABLE {}.{}".format(db_name,table_name)
+           )
+  print("Table dropped: {}.{}".format(db_name,table_name))
+
