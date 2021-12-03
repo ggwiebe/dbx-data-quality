ce43f5258d881086516111e794f321eca577b4d0
Glenn Wiebe <Glenn.Wiebe@databricks.com>
2021-12-03 07:35:33 -0500

    Add Queries for target and tracker

---
+++ sql/LoadTracker.sql
@@ -0,0 +1,3 @@
+SELECT *
+  FROM ggw_wine.Load_Track
+;
---
+++ sql/RedWineQuality.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT(load_dt || " - " || load_source || " - " || load_component) 
+    --   , DISTINCT(load_source), DISTINCT(load_component)
+  FROM ggw_wine.quality_red_wine
+;
