73fbdcbad4e3e1d9128fdc006b70c3e429493b77
ggwiebe <ggwiebe>
2021-12-03 13:28:21 +0000

    Add new profile query

--- sql/RedWineQuality.sql
+++ sql/RedWineQuality.sql
@@ -1,8 +1,15 @@
 -- Databricks notebook source
+-- Check unique loads
 SELECT DISTINCT(load_dt || " - " || load_source || " - " || load_component) 
     --   , DISTINCT(load_source), DISTINCT(load_component)
   FROM ggw_wine.quality_red_wine
 ;
+
+-- Simple count profile
+SELECT COUNT(*)
+  FROM ggw_wine.quality_red_wine
+;
+
 
 -- COMMAND ----------
 
