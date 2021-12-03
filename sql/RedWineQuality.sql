ed6ae06f48f059cf261d5f040c3bb66350e16995
Glenn Wiebe <Glenn.Wiebe@databricks.com>
2021-12-03 07:48:55 -0500

    Switch SQL file to SQL Notebook

--- sql/RedWineQuality.sql
+++ sql/RedWineQuality.sql
@@ -1,3 +1,4 @@
+-- Databricks notebook source
 SELECT DISTINCT(load_dt || " - " || load_source || " - " || load_component) 
     --   , DISTINCT(load_source), DISTINCT(load_component)
   FROM ggw_wine.quality_red_wine
